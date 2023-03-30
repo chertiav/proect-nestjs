0e3c84b6c32330386d3044cf4be4a2c9557a9461
andrey <chertiav@gmail.com>
2023-03-30 12:58:01 +0200

    API documentation. Adding the Auth Route to the Documentation

--- server/src/modules/auth/auth.controller.ts
+++ server/src/modules/auth/auth.controller.ts
@@ -1,4 +1,5 @@
 import { Body, Controller, Post } from '@nestjs/common';
+import { ApiResponse, ApiTags } from '@nestjs/swagger';
 import { CreateUserDTO } from '../user/dto';
 import { AuthService } from './auth.service';
 import { UserLoginDTO } from './dto';
@@ -8,11 +9,15 @@ import { AuthUserResponse } from './response';
 export class AuthController {
 	constructor(private readonly authService: AuthService) {}
 
+	@ApiTags('API')
+	@ApiResponse({ status: 201, type: CreateUserDTO })
 	@Post('register')
 	register(@Body() dto: CreateUserDTO): Promise<CreateUserDTO> {
 		return this.authService.registerUsers(dto);
 	}
 
+	@ApiTags('API')
+	@ApiResponse({ status: 200, type: AuthUserResponse })
 	@Post('login')
 	login(@Body() dto: UserLoginDTO): Promise<AuthUserResponse> {
 		return this.authService.loginUser(dto);
