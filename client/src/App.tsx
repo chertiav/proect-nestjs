5ee8524231d5ac544c988775291cf06573a1f2fc
andrey <chertiav@gmail.com>
2023-04-01 19:12:12 +0200

    create client - create PrivateRoute

---
+++ client/src/App.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+//=====================================================
+import './App.css';
+import Home from './components/home';
+import LoginPage from './components/auth/login';
+import PrivateRoute from './utils/router/privateRoute';
+
+function App() {
+	return (
+		<div className="App">
+			<Routes>
+				<Route element={<PrivateRoute />}>
+					<Route path="/" element={<Home />} />
+				</Route>
+				<Route path="login" element={<LoginPage />} />
+			</Routes>
+		</div>
+	);
+}
+
+export default App;
