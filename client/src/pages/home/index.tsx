8957c0677129ba2da7c312eda7ab277729249e2c
Andrey Chertykovtsev <chertiav@gmail.com>
2023-04-14 18:18:13 +0200

    Creating area chart component - Creating graph component - Adding option chart - Adding chart data

--- client/src/pages/home/index.tsx
+++ client/src/pages/home/index.tsx
@@ -1,17 +1,19 @@
 import React, { useCallback, useEffect, useMemo, useRef } from 'react';
+import { Box, Grid } from '@mui/material';
+//===============================================================
+import { useStyles } from './styles';
 import { getFavoriteAssets } from '../../store/thunks/assets';
 import { useAppDispatch, useAppSelector } from '../../utils/hook';
-import { Box, Grid } from '@mui/material';
-import { useStyles } from './styles';
+import AreaChart from '../../components/charts/area-chart';
 
-const Home = () => {
+const Home: React.FC = (): JSX.Element => {
 	const favoriteAssets: any[] = useAppSelector(
 		(state) => state.assets.favoriteAssets,
 	);
 	const favoriteAssetName = useMemo(() => ['bitcoin', 'ethereum'], []);
 	const newArray = favoriteAssets.filter(
 		(value, index, self) =>
-			index === self.findIndex((t: any) => t.name === value.name),
+			index === self.findIndex((t) => t.name === value.name),
 	);
 
 	const dispatch = useAppDispatch();
@@ -34,8 +36,9 @@ const Home = () => {
 	}, [favoriteAssetName, fetchData]);
 
 	const renderFavoriteBlock = newArray.map((element: any) => {
-		const currentPrice = element.data.prices[0];
-		const currentCap = element.data.market_caps[0];
+		const currentPrice = element.data.prices[element.data.prices.length - 1];
+		const currentCap =
+			element.data.market_caps[element.data.market_caps.length - 1];
 		return (
 			<Grid item key={element.name} xs={12} sm={6} lg={6}>
 				<Grid container className={classes.topCardItem}>
@@ -43,16 +46,18 @@ const Home = () => {
 						<h3 className={classes.assetName}>{element.name}</h3>
 						<div className={classes.itemDetails}>
 							<h3 className={classes.cardPrice}>
-								{currentPrice[1].toFixed(2)}
+								$ {parseFloat(currentPrice[1].toFixed(2)).toLocaleString()}
 							</h3>
 							<p className={classes.cardCapitalize}>
-								{currentCap[1].toFixed(0)}
+								$ {parseInt(currentCap[1].toFixed(0)).toLocaleString()}
 							</p>
 						</div>
 					</Grid>
 
 					<Grid item xs={12} sm={6} lg={6}>
-						<h5>Chart</h5>
+						<h5>
+							<AreaChart data={element.data.prices} />
+						</h5>
 					</Grid>
 				</Grid>
 			</Grid>
