49ec7124b811cd61f8ac52eaf8876f3625b37ce8
Andrey Chertykovtsev <chertiav@gmail.com>
2023-04-14 22:06:38 +0200

    Improving area chart component - Fixing array length from data - Improving option  chart - Adding trend  to change price

--- client/src/pages/home/index.tsx
+++ client/src/pages/home/index.tsx
@@ -5,6 +5,8 @@ import { useStyles } from './styles';
 import { getFavoriteAssets } from '../../store/thunks/assets';
 import { useAppDispatch, useAppSelector } from '../../utils/hook';
 import AreaChart from '../../components/charts/area-chart';
+import TrendUp from '../../assets/images/chart/trend-up.svg';
+import TrendDown from '../../assets/images/chart/trend-down.svg';
 
 const Home: React.FC = (): JSX.Element => {
 	const favoriteAssets: any[] = useAppSelector(
@@ -36,27 +38,48 @@ const Home: React.FC = (): JSX.Element => {
 	}, [favoriteAssetName, fetchData]);
 
 	const renderFavoriteBlock = newArray.map((element: any) => {
-		const currentPrice = element.data.prices[element.data.prices.length - 1];
-		const currentCap =
-			element.data.market_caps[element.data.market_caps.length - 1];
+		const { data, name, singleAsset } = element;
+		const currentPrice = singleAsset.map(
+			(element: any) => element.current_price,
+		);
+		const changePrice = singleAsset.map(
+			(element: any) => element.market_cap_change_percentage_24h,
+		);
+		const slicePrice = data.map((element: number[]) => element[1]);
+		const avveragePrice =
+			slicePrice.reduce((price: number, total: number) => price + total) /
+			slicePrice.length;
 		return (
-			<Grid item key={element.name} xs={12} sm={6} lg={6}>
+			<Grid item key={name} xs={12} sm={6} lg={6}>
 				<Grid container className={classes.topCardItem}>
 					<Grid item xs={12} sm={6} lg={6} className={classes.containerItem}>
-						<h3 className={classes.assetName}>{element.name}</h3>
+						<h3 className={classes.assetName}>{name}</h3>
 						<div className={classes.itemDetails}>
 							<h3 className={classes.cardPrice}>
-								$ {parseFloat(currentPrice[1].toFixed(2)).toLocaleString()}
+								${currentPrice.toLocaleString()}
 							</h3>
-							<p className={classes.cardCapitalize}>
-								$ {parseInt(currentCap[1].toFixed(0)).toLocaleString()}
-							</p>
+							<Grid spacing={1} container className={classes.priceInfo}>
+								<Box className={changePrice > 0
+								? `${classes.priceTrend} ${classes.trendUp}`
+								: `${classes.priceTrend} ${classes.trendDown}`
+							}>
+									{changePrice > 0 ? (
+										<img src={TrendUp} alt="up" />
+									) : (
+										<img src={TrendDown} alt="down" />
+									)}
+									<span>{parseFloat(changePrice).toFixed(2)}%</span>
+								</Box>
+								<span className={classes.avveragePrice}>
+									$ {avveragePrice.toLocaleString()}
+								</span>
+							</Grid>
 						</div>
 					</Grid>
 
 					<Grid item xs={12} sm={6} lg={6}>
 						<h5>
-							<AreaChart data={element.data.prices} />
+							<AreaChart data={data} />
 						</h5>
 					</Grid>
 				</Grid>
