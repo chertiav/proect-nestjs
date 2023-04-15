921d39b61b425122c4e751d3e7b6cf7cc8de5d95
Andrey Chertykovtsev <chertiav@gmail.com>
2023-04-15 13:01:52 +0200

    Adding line chart - Creating line chart component - Adding real data to chart - Customization chart

--- client/src/pages/home/index.tsx
+++ client/src/pages/home/index.tsx
@@ -5,15 +5,17 @@ import { useStyles } from './styles';
 import { getFavoriteAssets } from '../../store/thunks/assets';
 import { useAppDispatch, useAppSelector } from '../../utils/hook';
 import AreaChart from '../../components/charts/area-chart';
+import LineChart from '../../components/charts/line-chart';
 import TrendUp from '../../assets/images/chart/trend-up.svg';
 import TrendDown from '../../assets/images/chart/trend-down.svg';
+import { IChartData } from '../../common/types/assets';
 
 const Home: React.FC = (): JSX.Element => {
-	const favoriteAssets: any[] = useAppSelector(
+	const favoriteAssets: IChartData[] = useAppSelector(
 		(state) => state.assets.favoriteAssets,
 	);
 	const favoriteAssetName = useMemo(() => ['bitcoin', 'ethereum'], []);
-	const newArray = favoriteAssets.filter(
+	const filteredArray = favoriteAssets.filter(
 		(value, index, self) =>
 			index === self.findIndex((t) => t.name === value.name),
 	);
@@ -37,60 +39,72 @@ const Home: React.FC = (): JSX.Element => {
 		fetchData(favoriteAssetName);
 	}, [favoriteAssetName, fetchData]);
 
-	const renderFavoriteBlock = newArray.map((element: any) => {
-		const { data, name, singleAsset } = element;
-		const currentPrice = singleAsset.map(
-			(element: any) => element.current_price,
-		);
-		const changePrice = singleAsset.map(
-			(element: any) => element.market_cap_change_percentage_24h,
-		);
-		const slicePrice = data.map((element: number[]) => element[1]);
-		const avveragePrice =
-			slicePrice.reduce((price: number, total: number) => price + total) /
-			slicePrice.length;
-		return (
-			<Grid item key={name} xs={12} sm={6} lg={6}>
-				<Grid container className={classes.topCardItem}>
-					<Grid item xs={12} sm={6} lg={6} className={classes.containerItem}>
-						<h3 className={classes.assetName}>{name}</h3>
-						<div className={classes.itemDetails}>
-							<h3 className={classes.cardPrice}>
-								${currentPrice.toLocaleString()}
-							</h3>
-							<Grid spacing={1} container className={classes.priceInfo}>
-								<Box className={changePrice > 0
-								? `${classes.priceTrend} ${classes.trendUp}`
-								: `${classes.priceTrend} ${classes.trendDown}`
-							}>
-									{changePrice > 0 ? (
-										<img src={TrendUp} alt="up" />
-									) : (
-										<img src={TrendDown} alt="down" />
-									)}
-									<span>{parseFloat(changePrice).toFixed(2)}%</span>
-								</Box>
-								<span className={classes.avveragePrice}>
-									$ {avveragePrice.toLocaleString()}
-								</span>
-							</Grid>
-						</div>
-					</Grid>
+	const renderFavoriteBlock: JSX.Element[] = filteredArray.map(
+		(element: any) => {
+			const { price_chart_data, name, singleAsset } = element;
+			const currentPrice = singleAsset.map(
+				(element: any) => element.current_price,
+			);
+			const changePrice = singleAsset.map(
+				(element: any) => element.market_cap_change_percentage_24h,
+			);
+			const slicePrice = price_chart_data.map(
+				(element: number[]) => element[1],
+			);
+			const avveragePrice =
+				slicePrice.reduce((price: number, total: number) => price + total) /
+				slicePrice.length;
+			return (
+				<Grid item key={name} xs={12} sm={6} lg={6}>
+					<Grid container className={classes.topCardItem}>
+						<Grid item xs={12} sm={6} lg={6} className={classes.containerItem}>
+							<h3 className={classes.assetName}>{name}</h3>
+							<div className={classes.itemDetails}>
+								<h3 className={classes.cardPrice}>
+									${currentPrice.toLocaleString()}
+								</h3>
+								<Grid spacing={1} container className={classes.priceInfo}>
+									<Box
+										className={
+											changePrice > 0
+												? `${classes.priceTrend} ${classes.trendUp}`
+												: `${classes.priceTrend} ${classes.trendDown}`
+										}
+									>
+										{changePrice > 0 ? (
+											<img src={TrendUp} alt="up" />
+										) : (
+											<img src={TrendDown} alt="down" />
+										)}
+										<span>{parseFloat(changePrice).toFixed(2)}%</span>
+									</Box>
+									<span className={classes.avveragePrice}>
+										$ {avveragePrice.toLocaleString()}
+									</span>
+								</Grid>
+							</div>
+						</Grid>
 
-					<Grid item xs={12} sm={6} lg={6}>
-						<h5>
-							<AreaChart data={data} />
-						</h5>
+						<Grid item xs={12} sm={6} lg={6}>
+							<h5>
+								<AreaChart data={price_chart_data} />
+							</h5>
+						</Grid>
 					</Grid>
 				</Grid>
-			</Grid>
-		);
-	});
+			);
+		},
+	);
 
 	return (
 		<Box className={classes.root}>
-			<Grid container spacing={2}>
+			<Grid container spacing={2} className={classes.areaChart}>
 				{renderFavoriteBlock}
+			</Grid>
+			<Grid container className={classes.lineChartBlock}>
+				<Grid item xs={12} sm={12} lg={12}>
+					{filteredArray.length && <LineChart data={filteredArray} />}
+				</Grid>
 			</Grid>
 		</Box>
 	);
